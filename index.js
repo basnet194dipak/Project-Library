4930c2da6ab7f2c57b4c7c584863b004bccc2e54
basnet194dipak <264971136+basnet194dipak@users.noreply.github.com>
2026-09-20 11:13:25 +0000

    Loop through the myLibrary array and display book in library class in html
    User can now see the book in library

--- index.js
+++ index.js
@@ -23,3 +23,32 @@ function addBookToLibrary(title, author, pages, read = true) {
     let book = new Book(title, author, pages, read)
     myLibrary.push(book)
 }
+
+addBookToLibrary("Think Fast and Slow", "J.R.R. Tolkien", 310, true);
+addBookToLibrary("You can Win", "J.R.R. Tolkien", 320, false);
+addBookToLibrary("Think and Grow Rich", "J.R.R. Tolkien", 330, true);
+addBookToLibrary("How to influence people and win friends", "J.R.R. Tolkien", 310, true);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, false);
+
+// loop through the mylibrary
+document.addEventListener("DOMContentLoaded", function () {
+    let library = document.querySelector(".library")
+    for (let i = 0; i < myLibrary.length; i++) {
+        let book = myLibrary[i]
+        let display_book = document.createElement("div")
+        if (book.read == true) {
+            read = "read"
+        }
+        else {
+            read = "not read"
+        }
+        display_book.setAttribute("class", "book")
+        display_book.innerText = `id:${book["id"]}
+                                title:${book["title"]}
+                                author:${book["author"]}
+                                pages:${book["pages"]}
+                                read:${read}`
+
+        library.appendChild(display_book)
+    }
+})
